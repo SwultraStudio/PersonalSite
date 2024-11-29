920ed88c95685c46c534fce38a2647ef81a4a27b
SwultraStudio <swultrastudio@gmail.com>
2024-11-29 16:19:58 +0300

    remove dark

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -21,11 +21,11 @@ const Home = () => {
             <section dir="rtl" class="container px-6 py-16 mx-auto text-center">
                 <div class="max-w-4xl mx-auto">
                     <h1 class="text-3xl font-semibold text-gray-800 dark:text-white lg:text-5xl">نبذة عن احمد محمد</h1>
-                    <p class="mt-6 text-blue-600 dark:text-gray-300 text-lg">
+                    <p class="mt-6 text-blue-600  text-lg">
                     Full-Stack Developer | Mobile & Desktop App Developer | DevOps | Content Creator
 
                     </p>
-                    <p class="mt-4 text-gray-500 dark:text-gray-300 text-xl">
+                    <p class="mt-4 text-gray-500  text-xl">
                         أهلاً وسهلاً بكم في موقعي الشخصي! هنا ستجدون معلومات حول مهاراتي، مشاريعي، والمحتوى التعليمي الذي أقدمه. استكشفوا واستمتعوا بمعرفة المزيد عن رحلتي في عالم البرمجة.
                     </p>
                     <HashLink smooth to="/#Skills"><button class="group w-fit px-3 pr-4 py-2 mt-6 text-base font-semibold tracking-wider text-black uppercase transition-colors duration-200 transform bg-yellow-400 rounded-lg lg:w-auto hover:bg-green-600 focus:outline-none animate-bounce shadow-md hover:text-white">ابدء الاكتشاف <Icon className="animate-spin mr-4 text-gray-950 group-hover:text-white transition duration-200" iconSet={iconSet} icon="explore" size={30} aria-hidden="true"/></button></HashLink>
